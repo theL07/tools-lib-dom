bda2aadd8abb6798a08d9b58315e89c3ba8e2d61
bianshichun <sc.bian@gempoll.com>
2022-06-13 21:20:48 +0800

    方法模块

--- index.js
+++ index.js
@@ -5,14 +5,7 @@
  * @LastEditTime: 2022-06-13 21:19:22
  * @LastEditors: theL07
  */
-import tools from './src/tools'
-
-
-const indexTools = {
-  ...tools
-}
-export default indexTools
-
-export {
-  tools
-}
+import tools from './src/tools';
+const indexTools = Object.assign({}, tools);
+export default indexTools;
+export { tools };
---
+++ types/tools.d.ts
@@ -0,0 +1,29 @@
+/*
+ * @Description: 声明文件
+ * @Author: theL07
+ * @Date: 2022-06-13 20:47:13
+ * @LastEditTime: 2022-06-13 21:00:30
+ * @LastEditors: theL07
+ */
+export as namespace ToolTypes
+
+export interface Coordinates {
+  lat: number
+  lng: number
+}
+export interface CoordDistance {
+  startCoord: Coordinates
+  endCoord: Coordinates
+}
+
+export interface FormatTimeType {
+  date: Date;
+  format?: 'yyyy-MM-dd' | 'yyyy-MM-dd HH:mm:ss' | 'HH:mm:ss'
+  link?: '-' | '/'
+}
+
+export interface WxTypes {
+  miniProgram: {
+    getEnv: (arg0: (res: { miniprogram?: unknown; }) => void) => void
+  }
+}
